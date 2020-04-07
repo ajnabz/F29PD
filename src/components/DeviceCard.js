3d1f88bd9b6697be6cea006f386a3155753895ef
connor-troup98 <56512127+connor-troup98@users.noreply.github.com>
2020-04-07 19:27:18 +0100

    Device settings added

--- src/components/DeviceCard.js
+++ src/components/DeviceCard.js
@@ -25,12 +25,48 @@ function DeviceCard(props) {
                             <Card style={{ width: '100%' }}>
                                 <Card.Body>
                                     <Card.Title>{house.rooms[0].devices[0].device_name} <Toggle /></Card.Title>
-                                    <Card.Text>
-                                        <ListGroup style={{ paddingTop: '1.5%' }}>
-                                            <ListGroup.Item>Delete Device</ListGroup.Item>
-                                            <ListGroup.Item>Manage Device</ListGroup.Item>
-                                        </ListGroup>
-                                    </Card.Text>
+                                <Card.Text>
+                                    <Tabs defaultActiveKey="data" transition={false} id="noanim-tab-example">
+                                        <Tab eventKey="data" title="Data">
+                                            <ChartsPageDevices />
+                                        </Tab>
+                                        <Tab eventKey="settings" title="Manage Device">
+                                            <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+                                                <Nav variant="pills" defaultActiveKey="/home">
+                                                    <Nav.Item>
+                                                        <Nav.Link href="#link0">Delete Device</Nav.Link>
+                                                    </Nav.Item>
+                                                    <Nav.Item>
+                                                        <Nav.Link eventKey="#link1">Change Room</Nav.Link>
+                                                    </Nav.Item>
+                                                    <Nav.Item>
+                                                        <Nav.Link eventKey="#link2">Device Permissions</Nav.Link>
+                                                    </Nav.Item>
+                                                </Nav>
+                                                <Tab.Content>
+                                                    <Tab.Pane eventKey="#link0">
+                                                        <p>Once deleted, the devices and data connected to this room will be lost.</p>
+                                                        <br></br>
+                                                        <Button>Delete</Button>
+                                                    </Tab.Pane>
+                                                    <Tab.Pane eventKey="#link1">
+                                                        <Form>
+                                                            <Form.Group controlId="formBasicEmail">
+                                                                <Form.Control type="name" placeholder="Enter the new room ID" />
+                                                                <Form.Text className="text-muted"> This will be the new room that the device is attached to. </Form.Text>
+                                                            </Form.Group>
+                                                        </Form>
+                                                        <Button>Save</Button>
+                                                    </Tab.Pane>
+                                                    <Tab.Pane eventKey="#link2">
+                                                        <p>To fill in later</p>
+                                                        <Button>Save</Button>
+                                                    </Tab.Pane>
+                                                </Tab.Content>
+                                            </Tab.Container>
+                                        </Tab>
+                                    </Tabs>
+                                </Card.Text>
                                 </Card.Body>
                             </Card>
                             <br></br>
